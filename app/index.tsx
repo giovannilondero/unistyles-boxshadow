2a252a8bfb686bb260a21984268bc1abf520bcf3
Giovanni Londero <giovanni@visup.it>
2024-12-18 22:01:35 +0100

    cleanup unused imports

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
-import { Image, Platform, View } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { View } from "react-native";
 import { StyleSheet } from "react-native-unistyles";
 
 export default function HomeScreen() {
-  return (
-      <View style={styles.testBoxShadow} />
-  );
+  return <View style={styles.testBoxShadow} />;
 }
 
 const styles = StyleSheet.create({
